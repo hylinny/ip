46788608d2257e8b07720ffb8325a2ee5ae9482d
hylinny <hylin899@gmail.com>
2025-08-20 21:03:53 +0800

    Added support for Todos, Events and Deadlines.

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -14,6 +14,7 @@ public class John {
         while (!input.equals("bye")) {
             John.printLine();
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 tasklist.printTasks();
             } else if (input.startsWith("mark")) {
                 int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
@@ -28,8 +29,33 @@ public class John {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(task);
             } else {
-                System.out.println("added: " + input);
-                tasklist.addTask(new Task(input));
+                String[] parts = input.split(" ", 2);
+                String command = parts[0];
+                String description = parts[1];
+                if (command.equals("todo")) {
+                    Todo todo = new Todo(description);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(todo);
+                    tasklist.addTask(todo);
+                } else if (command.equals("deadline")) {
+                    String desc = description.split(" /by ", 2)[0];
+                    String endDate = description.split(" /by ", 2)[1];
+                    Deadline deadline = new Deadline(desc, endDate);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
+                    tasklist.addTask(deadline);
+                } else if (command.equals("event")) {
+                    String desc = description.split(" /from ", 2)[0];
+                    String startDate = description.split(" /from ", 2)[1].split(" /to ", 2)[0];
+                    String endDate = description.split(" /to ", 2)[1];
+                    Event event = new Event(desc, startDate, endDate);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
+                    tasklist.addTask(event);
+                } else {
+                    System.out.println("I'm sorry, I don't understand that command.");
+                }
+                System.out.println("You now have " + tasklist.getSize() + " tasks in the list.");
             }
             John.printLine();
             System.out.println();
