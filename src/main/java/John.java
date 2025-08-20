4fc14291921351c5a3139c3b99f47e418d45689f
hylinny <hylin899@gmail.com>
2025-08-20 14:00:43 +0800

    refactor code to use Task class

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class John {
-    public List<String> tasks = new ArrayList<>();
+    protected List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,11 +18,11 @@ public class John {
             John.printLine();
             if (input.equals("list")) {
                 for (int i = 0; i < john.tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + john.tasks.get(i));
+                    System.out.println((i + 1) + ". " + john.tasks.get(i).description);
                 }
             } else {
                 System.out.println("added: " + input);
-                john.tasks.add(input);
+                john.tasks.add(new Task(input));
             }
             John.printLine();
             System.out.println();
